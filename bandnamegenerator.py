62ee608db29963d8b75c337da843f7c85ea46a8c
bhanumalhotra92 <bhanu.malhotra@karglobal.com>
2023-03-11 13:03:57 +0530

    bandgenerator

--- bandnamegenerator.py
+++ bandnamegenerator.py
@@ -1,5 +1,7 @@
 print("Welcome to the Band Name Generator")
+
 name_of_city = input("What's name of the city you grew up in?" )
+
 print(name_of_city)
 
 pet_name = input("What's your pet's name? ")
